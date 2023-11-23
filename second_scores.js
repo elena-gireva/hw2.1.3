65bfa39582026af51f6dbe3e80af31e6d96cedc2
ElenaGireva <elena.webcode@gmail.com>
2023-11-23 20:27:28 +0100

    added second_scores js file

---
+++ second_scores.js
@@ -0,0 +1,19 @@
+let value1 = 2;
+let value2;
+let max = 3;
+
+
+function getRandomInt(max) {
+    return value2 = Math.floor(Math.random() * max);
+}
+
+value2 = getRandomInt(max);
+
+console.log (value1);
+console.log (value2);
+console.log (value1 + value2 / 5 + 17);
+
+
+
+
+
