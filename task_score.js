aede3e45ddbb76463335305dbb33421b10682ef2
ElenaGireva <elena.webcode@gmail.com>
2023-11-23 19:49:47 +0100

    add code in js file

--- task_score.js
+++ task_score.js
@@ -1,1 +1,14 @@
 console.log("Hi!!!");
+
+let score_vvedenie = 250;
+let score_git = 180;
+let score_js = 200;
+
+let score_average = (score_vvedenie + score_git + score_js) / 3;
+
+let text_average_score = "Средний балл за первые три модуля " + score_average + " баллов";
+
+let surname = "Gireva";
+let ferstame = "Elena";
+
+console.log(text_average_score + " у студента " + ferstame +  " " + surname);
